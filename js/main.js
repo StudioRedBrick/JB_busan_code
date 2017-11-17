6b26e298f4458411b5fb2033e271d01efb48181e
subinyoon <sbyoon92@gmail.com>
2017-11-17 10:54:43 +0900

    17.11.17 subin commit

--- js/main.js
+++ js/main.js
@@ -3,22 +3,107 @@ $(function(){
         
     //alert("hello");
     
-    $(".menu1").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".txtbox1").offset().top},
-        'slow');
+    //blog contents_2 mouseover start
+    $('.blog_contents_2').on('mouseover',function(){
+        //실행
+        $('.blog_contents_2_inner').css({
+            'display':'block'
+        });
     });
+    //blog contents_2 mouseover end
     
-    $(".menu2").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".txtbox2").offset().top},
-        'slow');
-    });
     
-    $(".menu3").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".txtbox3").offset().top},
-        'slow');
+    //blog contents_2 mouseout start
+    $('.blog_contents_2').on('mouseout',function(){
+        //실행
+        $('.blog_contents_2_inner').css({
+            'display':'none'
+        });
     });
+    //blog contents_2 mouseout end
+    
+    //---------------------------------------------------------------
+    
+    //blog contents_3 mouseover start
+    $('.blog_contents_3').on('mouseover',function(){
+        //실행
+        $('.blog_contents_3_inner').css({
+            'display':'block'
+        });
+    });
+    //blog contents_3 mouseover end
+    
+    
+    //blog contents_3 mouseout start
+    $('.blog_contents_3').on('mouseout',function(){
+        //실행
+        $('.blog_contents_3_inner').css({
+            'display':'none'
+        });
+    });
+    //blog contents_3 mouseout end
+    
+    //---------------------------------------------------------------
+    
+    //blog contents_4 mouseover start
+    $('.blog_contents_4').on('mouseover',function(){
+        //실행
+        $('.blog_contents_4_inner').css({
+            'display':'block'
+        });
+    });
+    //blog contents_4 mouseover end
+    
+    
+    //blog contents_4 mouseout start
+    $('.blog_contents_4').on('mouseout',function(){
+        //실행
+        $('.blog_contents_4_inner').css({
+            'display':'none'
+        });
+    });
+    //blog contents_4 mouseout end
+    
+    //---------------------------------------------------------------
+    
+    //blog contents_5 mouseover start
+    $('.blog_contents_5').on('mouseover',function(){
+        //실행
+        $('.blog_contents_5_inner').css({
+            'display':'block'
+        });
+    });
+    //blog contents_5 mouseover end
+    
+    
+    //blog contents_5 mouseout start
+    $('.blog_contents_5').on('mouseout',function(){
+        //실행
+        $('.blog_contents_5_inner').css({
+            'display':'none'
+        });
+    });
+    //blog contents_5 mouseout end
+    
+    //---------------------------------------------------------------
+    
+    //blog contents_6 mouseover start
+    $('.blog_contents_6').on('mouseover',function(){
+        //실행
+        $('.blog_contents_6_inner').css({
+            'display':'block'
+        });
+    });
+    //blog contents_6 mouseover end
+    
+    
+    //blog contents_6 mouseout start
+    $('.blog_contents_6').on('mouseout',function(){
+        //실행
+        $('.blog_contents_6_inner').css({
+            'display':'none'
+        });
+    });
+    //blog contents_6 mouseout end
     
 });//END
