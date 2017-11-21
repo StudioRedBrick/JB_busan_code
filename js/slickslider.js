fc1b45fad05b8bfdb251d48128b8e2cdde4866c7
yeeunsung <sungye727@gmail.com>
2017-11-21 16:49:39 +0900

    171121_ye

--- js/slickslider.js
+++ js/slickslider.js
@@ -54,6 +54,8 @@
               // instead of a settings object
                 ]
                 
+                
+                
             });
  
                        
@@ -66,6 +68,58 @@
             });
             
             
+            
+             $('.multi_slider').slick({
+                accessibility:false,
+                slidesToShow:4,
+                infinite: true,
+                slidesToScroll: 1,
+                centerMode: false,
+                /* 컨텐츠의 위아래 여백조절*/
+                centerPadding: '0px',
+                autoplay: false,
+                autoplaySpeed: 1500,
+                
+                arrows: false,
+                /* 반응형 */
+                responsive: [
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2,
+                            infinite: true,
+                            dots: false,
+                        }
+                    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
+                    }
+              // You can unslick at a given breakpoint now by adding:
+              // settings: "unslick"
+              // instead of a settings object
+                ]
+                
+            });
+            
+            $('.slick-prev').click(function(){
+               $('.multi_slider').slick('slickPrev'); 
+            });
+            
+            $('.slick-next').click(function(){
+               $('.multi_slider').slick('slickNext'); 
+            });
                 
 //            
 //                 // custom vertical slider highlight center slide
